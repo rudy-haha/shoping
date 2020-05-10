ff91e8175bd6d59ed8382826d4e9adc72c008367
Rudy-haha <3513668718@qq.com>
2020-05-10 19:46:09 +0800

    new branch

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './css/global.css'
 import './iconfont/iconfont.css'
-
+// 注册ElementUI
 Vue.use(ElementUI)
 new Vue({
   el:'#app',
